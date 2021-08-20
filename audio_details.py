11703c684745b36183e8f75dab987975ff90f3f0
MANUEL ADRIAN CHAVEZ SANCHEZ <manuel.adrian.chavez@ibm.com>
2021-08-20 13:08:54 -0500

    Integrate db api that stores results

---
+++ audio_details.py
@@ -0,0 +1,15 @@
+import requests
+import os
+word_count_service = os.environ.get('WORD_COUNTER_SERVICE', 'http://localhost:5000/api')
+deliver_assignment_endpoint = os.environ.get('DELIVER_ASSIGNMENT_ENDPOINT','/deliver-assignments')
+telegram_token = os.environ['TELEGRAM_TOKEN']
+telegram_audio_endpoint = os.environ.get('TELEGRAM_AUDIO_ENDPOINT','https://api.telegram.org/file/bot')
+
+def save_audio_deliver(assignment_id, student_id, file_name, total_words_detected):
+	r = requests.post(word_count_service+deliver_assignment_endpoint, json={
+	"assignment_id": assignment_id,
+	"student_id": student_id,
+	"audio_URL": telegram_audio_endpoint + telegram_token + '/' + file_name,
+	"total_words_detected": total_words_detected
+	})
+	return r.json()
--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
 import telebot
 import json
 import os
+import re
 from audio_helper import get_format, save_audio, count_words
+from audio_details import save_audio_deliver
 telegram_token = os.environ['TELEGRAM_TOKEN']
+success_response = os.environ.get('SUCCESS_RESPONSE', 'Assignment delivered successfully')
+response_message = os.environ.get('RESPONSE_MESSAGE', 'Tarea recibida.')
+failure_message = os.environ.get('FAILURE_MESSAGE', 'Algo salia mal con la tarea, por favor intentar enviarla nuevamente.')
+
+
 bot = telebot.TeleBot(telegram_token)
 
 @bot.message_handler(commands=['start', 'help'])
@@ -15,22 +22,27 @@ def echo_all(message):
 
 @bot.message_handler(content_types=['audio'])
 def handle_audio(message):
-    bot.reply_to(message, "procesando audio ...")
-    file_info = bot.get_file(message.audio.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    
-    sound = save_audio(downloaded_file,message.from_user.id,message.date,message.audio.file_unique_id,message.audio.mime_type)
-    count = count_words(sound)
-    bot.reply_to(message, count + " palabras fueron detectadas")
+	file_info = bot.get_file(message.audio.file_id)
+	downloaded_file = bot.download_file(file_info.file_path)
+
+	sound = save_audio(downloaded_file,message.from_user.id,message.date,message.audio.file_unique_id,message.audio.mime_type)
+	count = count_words(sound)
+	response = save_audio_deliver(None, message.from_user.id, file_info.file_path, count)
+	if re.match(re.escape(success_response),response['message']):
+		bot.reply_to(message, response_message)
+	else:
+		bot.reply_to(message, failure_message)
 
 @bot.message_handler(content_types=['voice'])
 def handle_voice(message):
-    bot.reply_to(message, "procesando voz ...")
-    file_info = bot.get_file(message.voice.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    sound = save_audio(downloaded_file,message.from_user.id,message.date,message.voice.file_unique_id,message.voice.mime_type)
-    count = count_words(sound)
-    bot.reply_to(message, count + " palabras fueron detectadas")
+	file_info = bot.get_file(message.voice.file_id)
+	downloaded_file = bot.download_file(file_info.file_path)
+	sound = save_audio(downloaded_file,message.from_user.id,message.date,message.voice.file_unique_id,message.voice.mime_type)
+	count = count_words(sound)
+	response = save_audio_deliver(None, message.from_user.id, file_info.file_path, count)
+	if re.match(re.escape(success_response),response['message']):
+		bot.reply_to(message, response_message)
+	else:
+		bot.reply_to(message, failure_message)
 
 bot.polling()
-
